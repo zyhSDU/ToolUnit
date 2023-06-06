f1f362ba9cada8e95ccc9509c6846f7f2fab4283
zyhSDU <34438397+zyhSDU@users.noreply.github.com>
2023-06-06 20:20:47 +0800

    cycle1

--- src/test/java/helper/scxml/scxml2/t7_cycle/EnvHelper.kt
+++ src/test/java/helper/scxml/scxml2/t7_cycle/EnvHelper.kt
@@ -3,12 +3,10 @@ package helper.scxml.scxml2.t7_cycle
 import helper.base.LHMHelper.A3LHM
 import helper.scxml.ScxmlVarHelper.ClockConstraint
 import helper.scxml.scxml2.EnvHelper
-import helper.scxml.scxml2.Expand.DataExpand.exprToInt
 import helper.scxml.scxml2.SCXMLTuple
 import helper.scxml.scxml2.Scxml2Helper
 import helper.scxml.scxml2.StrategyTripleHelper
 import helper.scxml.scxml2.StrategyTripleHelper.IRenEventSelector
-import org.apache.commons.scxml2.model.Data
 
 object EnvHelper {
     class Env(
@@ -20,21 +18,11 @@ object EnvHelper {
         envEventLHM,
         getIRenEventSelectorFun,
     ) {
-        override val scxmlTuple = Scxml2Helper.getSCXMLTuple("scxml2/t7_cycle/cycle.scxml").also {
+        override val scxmlTuple = Scxml2Helper.getSCXMLTuple("scxml2/t7_cycle/cycle1.scxml").also {
             it.initialStateList.add("s0")
             it.finalStateList.add("s1")
             it.stateNeedAddOneClockListLHM["s0"] = arrayListOf("x")
         }
-
-        val dataX: Data
-            get() {
-                return dataSCXML.getData("x")!!
-            }
-
-        val dataXInt: Int
-            get() {
-                return dataX.exprToInt()
-            }
     }
 
     fun getEnvObj1(): Env {
